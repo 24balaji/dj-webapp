39bd140d504dfe878ef9d5455919233768ced977
24balaji <99334076+24balaji@users.noreply.github.com>
2022-11-06 17:09:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
+song="";
+song1 ="";
+
 function preload()
 {
-
+    song =loadSound("music.mp3");
+    song1 = loadSound("music2.mp3")
 }
+
 
 function setup()
 {
 canvas = createCanvas(450, 350);
 canvas.center();
 video = createCapture(VIDEO);
-video.size(350,300);
+video.size(450,300);
+video.hide();
 }
 
-
-function draw() 
+function draw()
 {
-
+    image(video,0,0,600,500);
 }
+
